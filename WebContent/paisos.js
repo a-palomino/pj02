5e2d9fdca35708232bc1e137db2d95918caa334a
recioguti <e.recio@sapalomera.cat>
2021-01-21 16:57:04 +0100

    index

---
+++ WebContent/paisos.js
@@ -0,0 +1,45 @@
+let continents = [
+    {
+        "id:": "1",
+        "continent": "Europa",
+        "paisos": ["pais1", "pais2", "pais3"],
+        "imatges": ["/ruta/foto.png", "/ruta/foto2.png"],
+        "preus": ["pr", "pr", "pr"]
+    },
+    {
+        "id:": "1",
+        "continent": "Asia",
+        "paisos": ["pais1", "pais2", "pais3"],
+        "imatges": ["/ruta/foto.png", "/ruta/foto2.png"],
+        "preus": ["pr", "pr", "pr"]
+    },
+    {
+        "id:": "1",
+        "continent": "África",
+        "paisos": ["pais1", "pais2", "pais3"],
+        "imatges": ["/ruta/foto.png", "/ruta/foto2.png"],
+        "preus": ["pr", "pr", "pr"]
+    },
+    {
+        "id:": "1",
+        "continent": "América",
+        "paisos": ["pais1", "pais2", "pais3"],
+        "imatges": ["/ruta/foto.png", "/ruta/foto2.png"],
+        "preus": ["pr", "pr", "pr"]
+    },
+    {
+        "id:": "1",
+        "continent": "Oceania",
+        "paisos": ["pais1", "pais2", "pais3"],
+        "imatges": ["/ruta/foto.png", "/ruta/foto2.png"],
+        "preus": ["pr", "pr", "pr"]
+    }
+];
+
+var continent = document.getElementById("continent");
+var pais = document.getElementById("pais");
+
+
+
+
+
